546cbf4960ed4bb09117d4700d330174fff2b715
r0nk <r0nk@simplecpu.com>
2023-01-15 10:51:58 -0600

    moved sfx to directory

--- synth.py
+++ synth.py
@@ -1,24 +1,32 @@
 from mitmproxy import ctx
+import re
 from pygame import mixer
 import _thread
 
 class Synth:
     def __init__(self):
         mixer.init()
-        self.chips_sfx=mixer.Sound("chips.wav")
-        self.laser_sfx=mixer.Sound("portal.wav")
-        self.chips_sfx.set_volume(0.5)
-        self.laser_sfx.set_volume(0.5)
+        self.chips_sfx=mixer.Sound("sfx/chips.wav")
+        self.laser_sfx=mixer.Sound("sfx/laser.wav")
+        self.hoop_sfx=mixer.Sound("sfx/hoop.wav")
+        self.stick_sfx=mixer.Sound("sfx/stick.wav")
+
+        self.chips_sfx.set_volume(0.3)
+        self.laser_sfx.set_volume(0.3)
+        self.hoop_sfx.set_volume(0.2)
+        self.stick_sfx.set_volume(0.2)
 
     def request(self, flow):
-        if flow.request.method != "GET":
-            self.laser_sfx.play()
-        else:
+        if flow.request.method == "GET":
             self.chips_sfx.play()
+        else:
+            self.laser_sfx.play()
 
-#    def response(self, flow):
-#        self.chips_sfx.play()
-
+    def response(self,flow):
+        if(re.search("javascript",str(flow.response.headers.get("content-type")))):
+            self.stick_sfx.play()
+        if(re.search("json",str(flow.response.headers.get("content-type")))):
+            self.hoop_sfx.play()
 
 addons = [
     Synth()
